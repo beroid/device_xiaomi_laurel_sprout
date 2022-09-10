1bc5bd847b4f01bf0597caecadf361a680651926
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-09-10 12:26:52 +0530

    laurel_sprout: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
