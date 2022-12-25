b61d7c9d02a1389e0aeb62a7327790f31bd6aeb6
beroid <beroid3@gmail.com>
2022-12-25 10:45:37 +0500

    laurel_sprout: Drop libhidl

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -246,8 +246,9 @@ endif
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport.vendor
